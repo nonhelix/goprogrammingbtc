881a99b217ebf932e77235ad72b9db66b7416751
Zhao Meng <nonhelix@Zhaos-MacBook-Pro.local>
2021-11-21 10:59:10 +0800

    1-4

--- goprogrammingbtc.go
+++ goprogrammingbtc.go
@@ -27,3 +27,17 @@ func (x FieldElement) Sub(y FieldElement) FieldElement {
 	z.num.Mod(z.num, x.prime)
 	return z
 }
+func (x FieldElement) Mul(y FieldElement) FieldElement {
+	if x.prime.Cmp(y.prime) != 0 {
+		panic("mismatched primes in fieldElement")
+	}
+	z := FieldElement{big.NewInt(int64(0)), x.prime}
+	z.num.Mul(x.num, y.num)
+	z.num.Mod(z.num, x.prime)
+	return z
+}
+func (x FieldElement) Pow(n *big.Int) FieldElement {
+	z := FieldElement{big.NewInt(int64(0)), x.prime}
+	z.num.Exp(x.num, n, x.prime)
+	return z
+}
--- goprogrammingbtc_test.go
+++ goprogrammingbtc_test.go
@@ -38,3 +38,34 @@ func TestExercise1_2(t *testing.T) {
 		t.Error("wrong res4", res4)
 	}
 }
+func TestExercise1_4(t *testing.T) {
+	prime := big.NewInt(int64(97))
+	x1 := FieldElement{big.NewInt(int64(95)), prime}
+	y1 := FieldElement{big.NewInt(int64(45)), prime}
+	z1 := FieldElement{big.NewInt(int64(31)), prime}
+	res1 := x1.Mul(y1).Mul(z1)
+	ans1 := big.NewInt(int64(23))
+	if res1.num.Cmp(ans1) != 0 {
+		t.Error("wrong res1", res1)
+	}
+	x2 := FieldElement{big.NewInt(int64(17)), prime}
+	y2 := FieldElement{big.NewInt(int64(13)), prime}
+	z2 := FieldElement{big.NewInt(int64(19)), prime}
+	a2 := FieldElement{big.NewInt(int64(44)), prime}
+	res2 := x2.Mul(y2).Mul(z2).Mul(a2)
+	ans2 := big.NewInt(int64(68))
+	if res2.num.Cmp(ans2) != 0 {
+		t.Error("wrong res2", res2)
+	}
+	x3 := FieldElement{big.NewInt(int64(12)), prime}
+	y3 := big.NewInt(int64(7))
+	z3 := FieldElement{big.NewInt(int64(77)), prime}
+	a3 := big.NewInt(int64(49))
+	one := x3.Pow(y3)
+	two := z3.Pow(a3)
+	res3 := one.Mul(two)
+	ans3 := big.NewInt(int64(63))
+	if res3.num.Cmp(ans3) != 0 {
+		t.Error("wrong res3", res3)
+	}
+}
